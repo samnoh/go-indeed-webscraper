99200461b9364c49dbdd7856fcceccaef6e72d3c
Sam Noh <samnoh21@outlook.com>
2020-02-20 21:30:30 +1300

    clean up

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import "github.com/samnoh/job_scrapper/jobscrapper"
 
-var baseURL string = "https://nz.indeed.com/jobs?q=python&limit=50"
-
 func main() {
-	jobscrapper.Scrap(baseURL)
+	jobscrapper.Scrap("https://nz.indeed.com/jobs?q=python&limit=50")
 }
