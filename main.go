1f2b7f7d6a7111868fa9705ee05f79b5a73b296a
Sam Noh <samnoh21@outlook.com>
2020-02-23 20:40:51 +1300

    add static file

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/samnoh/go-indeed-webscraper/controllers"
 )
 
 func main() {
+	// Echo instance
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Static("/static", "public/assets")
+
+	// Route
 	controllers.Controllers(e)
+
+	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
 }
